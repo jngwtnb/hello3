a275e4bd9ae310fe1e6ce1b4fe6493e083665cc0
junkwind <40288272+junkwind@users.noreply.github.com>
2018-07-29 17:19:40 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,133 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
 import '../node_modules/onsenui/css/onsenui.css';
 import '../node_modules/onsenui/css/onsen-css-components.css';
 import ons from 'onsenui';
-import {Page, Button, Toolbar} from 'react-onsenui';
+//import {Page, Button, Toolbar} from 'react-onsenui';
+import {Navigator, Page, Button, Toolbar, ToolbarButton, BackButton, Icon, Tab, Tabbar} from 'react-onsenui';
 
-class App extends Component {
+
+class TabPage3 extends React.Component {
   handleClick() {
-    ons.notification.alert('Hello world!');
+    ons.notification.alert('Hello, world!');
   }
 
   render() {
     return (
-      <Page>
+      <Page renderToolbar={() => 
         <Toolbar>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <div className="center">My title</div>
+          <div className="center">{this.props.title}</div>
         </Toolbar>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!?
+      }>
+ 
+        <p style={{padding: '0 15px'}}>
+          This is the <strong>{this.props.title}</strong> page!
         </p>
-        <Button onClick={this.handleClick}>Tap me!</Button>
-        </Page>  
+
+        <Button onClick={this.handleClick}>Push!</Button>
+      </Page>
+    )
+  }
+}
+
+class TabPage2 extends React.Component {
+  handleClick() {
+    ons.notification.alert('Hello, world!');
+  }
+
+  render() {
+    return (
+      <Page renderToolbar={() => 
+        <Toolbar>
+          <div className="center">{this.props.title}!!!</div>
+        </Toolbar>
+      }>
+ 
+        <p style={{padding: '0 15px'}}>
+          This is the <strong>{this.props.title}</strong> page!
+        </p>
+
+        <Button onClick={this.handleClick}>Push!</Button>
+      </Page>
+    )
+  }
+}
+
+
+class MainPage extends React.Component {
+  renderTabs() {
+    const sections = [
+      'Home',
+      'Comments',
+    ];
+ 
+    let tabs = sections.map((section) => {
+      return {
+        content: <TabPage3 key={section} title={section} />,
+        tab: <Tab key={section} label={section} />
+      };
+    });
+
+    tabs.push({
+      content: <TabPage2 key={'Settings'} title={'Settings'} />,
+      tab: <Tab key={'Settings'} label={'Settings'} />
+    });
+    
+    return tabs;
+  }
+ 
+  popPage() {
+    this.props.navigator.popPage();
+  }
+
+
+  render() {
+    return (
+      <Page renderToolbar={() =>
+        <Toolbar> 
+          <div className='left'><ToolbarButton onClick={this.popPage.bind(this)}>Back</ToolbarButton></div>
+        </Toolbar>
+      }>
+        <Tabbar
+          initialIndex={1}
+          renderTabs={this.renderTabs}
+          swipeable={true}
+        />
+      </Page>
+
     );
   }
 }
 
-export default App;
+class LoginPage extends React.Component {
+  pushPage() {
+    this.props.navigator.pushPage({component: MainPage});
+  }
+
+  render() {
+    return (
+      <Button onClick={this.pushPage.bind(this)}>login</Button>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  renderPage(route, navigator) {
+    const props = route.props || {};
+    props.navigator = navigator;
+
+    return React.createElement(route.component, props);
+  }
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{component: LoginPage}}
+        renderPage={this.renderPage}
+      />
+    );
+  }
+}
